0f42d4f2ec5b49313f3394c9e2dd60782cf0da12
Gohome <lgh1598@126.com>
2020-12-01 19:47:07 +0800

    Toolman

--- app/src/main/java/com/home/toolman/activity/WordsBookActivity.java
+++ app/src/main/java/com/home/toolman/activity/WordsBookActivity.java
@@ -5,9 +5,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -25,19 +28,40 @@ import static org.litepal.LitePalApplication.getContext;
 public class WordsBookActivity extends AppCompatActivity implements View.OnClickListener{
     private RecyclerView bookRecyclerView;
     private List<WordsBook> bookList=new ArrayList<>();
-
+    private EditText editTextSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_words_book);
         bookRecyclerView=findViewById(R.id.words_book);
         initWordsBook();
+        ImageView imageViewSearch=(ImageView)findViewById(R.id.button_search_selector_words);
+        ImageView deleteEditText=(ImageView)findViewById(R.id.button_delete_edit_text);
+        editTextSearch=(EditText)findViewById(R.id.edit_selector_words);
+        deleteEditText.setOnClickListener(this);
+        imageViewSearch.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-
+        switch(v.getId()){
+            case R.id.button_search_selector_words:
+                String searchWord=editTextSearch.getText().toString();
+                if (TextUtils.isEmpty(searchWord)){
+                    Toast.makeText(this, "请输入要查询的单词", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                bookList=DataSupport.where("origin like ?","%"+searchWord+"%").find(WordsBook.class);
+                WordsAdapter adapter=new WordsAdapter(bookList);
+                bookRecyclerView.setAdapter(adapter);
+                break;
+            case R.id.button_delete_edit_text:
+                editTextSearch.setText(null);
+                initWordsBook();
+                break;
+            default:
+        }
     }
 
     private void initWordsBook(){
